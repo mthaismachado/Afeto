ed52bb8d418d92d37248f0ab7a4aedcb6716d0b1
Ramon Alves <ramon.calou@hotmail.com>
2020-01-21 22:34:06 -0200

    Making layout

---
+++ app/src/main/java/com/team/afeto/Dashboard_preciso_ajuda.java
@@ -0,0 +1,14 @@
+package com.team.afeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard_preciso_ajuda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_preciso_ajuda);
+    }
+}
---
+++ app/src/main/java/com/team/afeto/MainActivity.java
@@ -0,0 +1,47 @@
+package com.team.afeto;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button queroAjudar;
+    private Button btn_procurando_ajuda;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        queroAjudar = findViewById(R.id.btn_quero_ajudar);
+        queroAjudar.setOnClickListener(quero_ajudar_activity);
+
+        btn_procurando_ajuda = findViewById(R.id.btn_procurando_ajuda);
+        btn_procurando_ajuda.setOnClickListener(quero_ajuda_activity);
+
+    }
+
+    private View.OnClickListener quero_ajudar_activity = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener quero_ajuda_activity = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), Dashboard_preciso_ajuda.class);
+            startActivity(intent);
+        }
+    };
+
+}
