adbe19e3b81eb4f65a10be7e2e6f1eea19d9deb0
Ramon Alves <ramon.calou@hotmail.com>
2020-01-28 21:35:30 -0200

    Imagem sendo comprimida, resta pedido de permissao de escrita

--- app/src/main/java/com/team/afeto/Activity/PerfilActivity.java
+++ app/src/main/java/com/team/afeto/Activity/PerfilActivity.java
@@ -1,37 +1,40 @@
 package com.team.afeto.Activity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.team.afeto.Helper.UsuarioSingleton;
 import com.team.afeto.Model.Usuario;
 import com.team.afeto.R;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -50,6 +53,7 @@ public class PerfilActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
     private FirebaseAuth mAuth;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,13 +74,12 @@ public class PerfilActivity extends AppCompatActivity {
 
         btn_Arrow_Back.setOnClickListener(arrow_back);
 
-        // Initialize Firebase Auth
+        // Initialize Firebase
         mAuth = FirebaseAuth.getInstance();
 
         db = FirebaseFirestore.getInstance();
 
-        //TODO: Ver a possibilidade de colcoar os ícones ao lado do TextView.
-        //TODO: Implementar o firebase Storage para upload de imagem(ver bibilioteca para fazer crop) e ver como referenciar a foto do storage no firestore.
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         preencherTela();
     }
@@ -92,10 +95,15 @@ public class PerfilActivity extends AppCompatActivity {
         lbl_ComoAjuda.setText(usuario.getComoAjuda().toString());
         if(usuario.getUri_perfil() != null){
             profile_Image.setImageURI(usuario.getUri_perfil());
+        }else{
+            try {
+                recuperaImagemPerfil();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
-
     private View.OnClickListener crop_perfil = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -121,37 +129,77 @@ public class PerfilActivity extends AppCompatActivity {
             CropImage.ActivityResult  result = CropImage.getActivityResult(data);
             if(resultCode == RESULT_OK){
                 Usuario usuario = UsuarioSingleton.getUsuario();
-                usuario.setUri_perfil(result.getUri());
+                Bitmap bitmap = null;
+                try {
+                    bitmap = Bitmap.createScaledBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), result.getUri()), 1024, 1024, true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Uri uri =  getImageUri(getApplicationContext(), bitmap);
+                usuario.setUri_perfil(uri);
+                gravarImagemPerfil(usuario, uri);
                 UsuarioSingleton.setUsuario(usuario);
-                profile_Image.setImageURI(result.getUri());
+                profile_Image.setImageURI(uri);
                 Toast.makeText(this, "Imagem atualizada com sucesso", Toast.LENGTH_SHORT).show();
             }
         }
 
     }
 
-    private void gravarImagemPerfil(final Usuario usuario){
+    private void gravarImagemPerfil(final Usuario usuario, Uri uri){
         FirebaseUser user = mAuth.getCurrentUser();
-        db.collection("users").document(user.getUid())
-                .set(usuario)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
+        Uri file = uri;
+        final StorageReference perfilRef = mStorageRef.child("perfil_images/" + user.getUid() + ".jpg");
 
+        perfilRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(PerfilActivity.this, "Não conseguimos salvar a sua foto :(", Toast.LENGTH_SHORT).show();
+                    public void onFailure(@NonNull Exception exception) {
+
                     }
                 });
+    }
+
+    private void recuperaImagemPerfil() throws IOException {
+        if(UsuarioSingleton.getUsuario().getUri_perfil() == null){
+            FirebaseUser user = mAuth.getCurrentUser();
+
+            final StorageReference perfilRef = mStorageRef.child("perfil_images/" + user.getUid() + ".jpg");
+
+            final File localFile = File.createTempFile(user.getUid(), "jpg", getCacheDir());
+
+            perfilRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Uri uri = Uri.fromFile(localFile);
+                    UsuarioSingleton.getUsuario().setUri_perfil(uri);
+                    profile_Image.setImageURI(uri);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                }
+            });
+        }
     }
 
     private void startCrop(Uri imageuri){
         CropImage.activity(imageuri)
                 .setMultiTouchEnabled(true)
                 .start(this);
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 
     private View.OnClickListener arrow_back = new View.OnClickListener() {
--- app/src/main/java/com/team/afeto/Activity/SplashActivity.java
+++ app/src/main/java/com/team/afeto/Activity/SplashActivity.java
@@ -3,22 +3,31 @@ package com.team.afeto.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.team.afeto.Helper.UsuarioSingleton;
 import com.team.afeto.Model.Usuario;
 import com.team.afeto.R;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class SplashActivity extends AppCompatActivity {
@@ -27,24 +36,29 @@ public class SplashActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private Usuario usuario;
+    private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        usuario = new Usuario();
+
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         final FirebaseUser currentUser = mAuth.getCurrentUser();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(currentUser != null){
+                if (currentUser != null) {
                     getUserInfo(currentUser);
                     startActivity(new Intent(getBaseContext(), Dashboard_area_logada.class));
                     finishAffinity();
-                }else {
+                } else {
                     startActivity(new Intent(getBaseContext(), MainActivity.class));
                     finish();
                 }
@@ -52,7 +66,7 @@ public class SplashActivity extends AppCompatActivity {
         }, 1500);
     }
 
-    private void getUserInfo(FirebaseUser currentUser){
+    private void getUserInfo(FirebaseUser currentUser) {
         DocumentReference docRef = db.collection("users").document(currentUser.getUid());
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -60,7 +74,6 @@ public class SplashActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        usuario = new Usuario();
                         usuario.setNome(document.getData().get("nome").toString());
                         usuario.setCpf(document.getData().get("cpf").toString());
                         usuario.setGenero(document.getData().get("genero").toString());
@@ -68,7 +81,12 @@ public class SplashActivity extends AppCompatActivity {
                         usuario.setEstado(document.getData().get("estado").toString());
                         usuario.setCidade(document.getData().get("cidade").toString());
                         String comoAjuda = document.getData().get("comoAjuda").toString();
-                        usuario.setComoAjuda(Arrays.asList(comoAjuda.substring(1, comoAjuda.length()-1).split(",")));
+                        usuario.setComoAjuda(Arrays.asList(comoAjuda.substring(1, comoAjuda.length() - 1).split(",")));
+//                        try {
+//                            recuperaImagemPerfil();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
                         UsuarioSingleton.setUsuario(usuario);
                     } else {
                         Log.d("TAG", "No such document");
@@ -76,6 +94,27 @@ public class SplashActivity extends AppCompatActivity {
                 } else {
                     Log.d("TAG", "get failed with ", task.getException());
                 }
+            }
+        });
+    }
+
+    private void recuperaImagemPerfil() throws IOException {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        final StorageReference perfilRef = mStorageRef.child("perfil_images/" + user.getUid() + ".jpg");
+
+        final File localFile = File.createTempFile("images", "jpg");
+
+        perfilRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                usuario.setUri_perfil(Uri.fromFile(localFile));
+                UsuarioSingleton.setUsuario(usuario);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                UsuarioSingleton.setUsuario(usuario);
             }
         });
     }
